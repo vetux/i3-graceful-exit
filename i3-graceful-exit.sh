357cfbe101129ca092d896e654a6ccce5b991abb
xenotux <xenotux@outlook.com>
2022-06-17 13:05:33 +0200

    Update comments

--- i3-graceful-exit.sh
+++ i3-graceful-exit.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# i3 by default kills all the processes which were started through the gui when exiting the window manager.
+# This script closes all visible windows gracefully (The same as clicking the x on a more conventional window manager) before exiting i3.
+# It should be assigned in .config/i3/config as the exit shortcut nagbar click action instead of 'i3-msg exit'
+
 # Depends on xdotool and wmctrl
 
 allWindows=$(xdotool search -onlyvisible "" 2> /dev/null)
